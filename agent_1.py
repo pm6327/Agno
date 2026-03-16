b2553b577bb4c1ea47adde7dcb0b96242b4ff752
Prakhar Maheshwari <prakharmaheshwari33@gmail.com>
2026-03-16 10:55:12 +0530

    Agno practise

---
+++ agent_1.py
@@ -0,0 +1,9 @@
+from agno.agent import Agent
+
+from agno.models.openai import OpenAIChat
+from dotenv import load_dotenv
+
+# load keys
+load_dotenv()
+
+# build the agent
---
+++ main.py
@@ -0,0 +1,5 @@
+import agno
+import openai
+
+print("Agno version:", agno.__version__)
+print("OpenAI version:", openai.__version__)
